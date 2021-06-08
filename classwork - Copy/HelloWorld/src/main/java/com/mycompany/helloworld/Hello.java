559086ca60b1adbfdbd4d2c6d70f963535869dd8
davinder.negi1 <davinder.negi1@gmail.com>
2021-06-08 13:42:52 -0700

    first commit

---
+++ classwork - Copy/HelloWorld/src/main/java/com/mycompany/helloworld/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author Davinder 
+ *email: davinder.negi1@gmail.com
+ *date: 2021-06-07
+ *purpose: My first Hello World program in an IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Davinder!");
+    }
+}
